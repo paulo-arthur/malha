5d57e4e36ade9ee0ff6bbf30163552a7d4a60145
Paulo Arthur <pauloart.hur670@gmail.com>
2026-08-31 22:18:14 -0300

    implementing multiple reqs

--- main.go
+++ main.go
@@ -1,106 +1,162 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"bytes"
 )
 
-func readFile(path string) string {
-	content, err := os.ReadFile(path)
-	
+func readSinglePayloadFromFile(filePath string) string {
+	fileContent, err := os.ReadFile(filePath)
+
 	if err != nil {
 		fmt.Printf("Failed to read the file: %s", err)
 	}
 
-	return string(content)
+	return string(fileContent)
 }
 
-func httpGETreq(url string, c chan int){
-	resp, err := http.Get(url)
+func readMultiplePayloadsFromFile(filePath string) ([]string, int) {
+	rawFileContent := readSinglePayloadFromFile(filePath)
+	var parsedObjectsList []map[string]interface{}
+
+	err := json.Unmarshal([]byte(rawFileContent), &parsedObjectsList)
+	if err != nil {
+		fmt.Println("Error converting multiple payload into a slice.", err)
+		return []string{}, 0
+	}
+
+	var formattedPayloadsList []string
+	for _, singleObject := range parsedObjectsList {
+		jsonBytes, _ := json.Marshal(singleObject)
+		formattedPayloadsList = append(formattedPayloadsList, string(jsonBytes))
+	}
+
+	fmt.Println(formattedPayloadsList)
+	return formattedPayloadsList, len(formattedPayloadsList)
+}
+
+func sendGetRequest(targetUrl string, statusCodeChannel chan int) {
+	response, err := http.Get(targetUrl)
 	if err != nil {
 		fmt.Println("Request failed: ", err)
-		c <- 0
+		statusCodeChannel <- 0
 		return
 	}
 
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	responseBody, err := io.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println("Failed to read body", err)
-		c <- 0
+		statusCodeChannel <- 0
 		return
 	}
 
-	fmt.Println("Status code: ", resp.StatusCode)
-	fmt.Println(string(body))
-	c <- resp.StatusCode
+	fmt.Println("Status code: ", response.StatusCode)
+	fmt.Println(string(responseBody))
+	statusCodeChannel <- response.StatusCode
 }
 
-func httpPOSTreq(url string, filepath string, c chan int) {
-	binary_payload := []byte(readFile(filepath))
-
-	req_structure, err := http.NewRequest("POST", url, bytes.NewBuffer(binary_payload))
+func sendPostRequest(targetUrl string, stringPayload string, statusCodeChannel chan int) {
+	requestStructure, err := http.NewRequest("POST", targetUrl, bytes.NewBuffer([]byte(stringPayload)))
 	if err != nil {
 		fmt.Print("Something went wrong...", err)
-		return 
+		return
 	}
 
-	//req_structure.Header.Set("")
+	//requestStructure.Header.Set("")
 
-	client := &http.Client{}
-	resp, err := client.Do(req_structure)
+	httpClient := &http.Client{}
+	response, err := httpClient.Do(requestStructure)
 	if err != nil {
 		fmt.Println("Something went wrong...", err)
-		return 
+		return
 	}
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	bodyBytes, err := io.ReadAll(resp.Body)
+	responseBody, err := io.ReadAll(response.Body)
 
-	fmt.Println("Status Code: ", resp.StatusCode)
-	fmt.Println(string(bodyBytes))
+	fmt.Println("Status Code: ", response.StatusCode)
+	fmt.Println(string(responseBody))
+}
+
+func requestWorker(targetUrl string, jobsChannel <-chan string, resultsChannel chan<- int) {
+	for currentJobPayload := range jobsChannel {
+		sendPostRequest(targetUrl, currentJobPayload, resultsChannel)
+	}
 }
 
 func main() {
-	fmt.Println("-- MALIA - Go Fuzzler")
+	fmt.Println("-- MALIA - Go Fuzzler --")
 
 	fmt.Printf("Enter the URL: ")
-	var URL string
-	fmt.Scan(&URL)
+	var targetUrl string
+	fmt.Scan(&targetUrl)
 
 	fmt.Printf("Enter the method: ")
-	var method string
-	fmt.Scan(&method)
+	var httpMethod string
+	fmt.Scan(&httpMethod)
 
 	fmt.Printf("Enter the number of simultaneous reqs: ")
-	var N int
-	fmt.Scan(&N)
+	var workerCount int
+	fmt.Scan(&workerCount)
 
-	switch method {
+	switch httpMethod {
 	case "GET":
-		c := make(chan int)
-		var results_array []int 
+		statusCodeChannel := make(chan int)
+		var statusCodesList []int
 
-		for i := 0; i < N; i++ {
-			go httpGETreq(URL, c)
+		for workerIndex := 0; workerIndex < workerCount; workerIndex++ {
+			go sendGetRequest(targetUrl, statusCodeChannel)
 		}
 
-		for i := 0; i < N; i++ {
-			results_array = append(results_array, <-c)
-		} 
+		for workerIndex := 0; workerIndex < workerCount; workerIndex++ {
+			statusCodesList = append(statusCodesList, <-statusCodeChannel)
+		}
 
 	case "POST":
-		fmt.Printf("Enter the path to the payload file: ")
-		var path string
-		fmt.Scan(&path)
-		
-		c := make(chan int)
-		httpPOSTreq(URL, path, c)
+		fmt.Println("Choose the payload type: [L]ist | [U]nic")
+		var attackType string
+		fmt.Scan(&attackType)
 
-		
+		fmt.Printf("Enter the path to the payload file: ")
+		var payloadFilePath string
+		fmt.Scan(&payloadFilePath)
+
+		switch attackType {
+		case "L":
+			jsonPayloadsList, totalPayloadsCount := readMultiplePayloadsFromFile(payloadFilePath)
+			if totalPayloadsCount < workerCount {
+				fmt.Println("More channels then requests detected. Reducing the number of channels to ", totalPayloadsCount)
+				workerCount = totalPayloadsCount
+			}
+
+			jobsChannel := make(chan string, totalPayloadsCount)
+			resultsChannel := make(chan int, totalPayloadsCount)
+
+			for _, singlePayload := range jsonPayloadsList {
+				jobsChannel <- singlePayload
+			}
+			close(jobsChannel)
+
+			for workerIndex := 0; workerIndex < workerCount; workerIndex++ {
+				go requestWorker(targetUrl, jobsChannel, resultsChannel)
+			}
+
+			for responseIndex := 0; responseIndex < len(payloads); responseIndex++ {
+				receivedStatus := <-resultsChannel
+				fmt.Println(receivedStatus)
+			}
+
+			fmt.Println(jsonPayload)
+		case "U":
+			statusCodeChannel := make(chan int)
+			rawPayloadContent := readSinglePayloadFromFile(payloadFilePath)
+			sendPostRequest(targetUrl, []byte(rawPayloadContent), statusCodeChannel)
+		}
 	}
 }
